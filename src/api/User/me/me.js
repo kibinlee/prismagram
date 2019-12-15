0ad9350e1124df110afaab11d627e0a2c04f67bd
John Lee <faker@Johns-MacBook-Pro.local>
2019-12-15 20:21:46 +0900

    isLiked Computed File

--- src/api/User/me/me.js
+++ src/api/User/me/me.js
@@ -13,10 +13,5 @@ export default {
         posts
       };
     }
-  },
-  User: {
-    fullName: (parent, __, { request }) => {
-      return `${parent.firstName} ${parent.lastName}`;
-    }
   }
 };
