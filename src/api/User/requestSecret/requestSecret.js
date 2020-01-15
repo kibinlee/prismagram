abef94387f6dc260a8d0a9eca55357d82b9fcc98
KBL <kibinlee19k@gmail.com>
2020-01-15 21:56:21 +0900

    Likes and Comments

--- src/api/User/requestSecret/requestSecret.js
+++ src/api/User/requestSecret/requestSecret.js
@@ -3,17 +3,15 @@ import { prisma } from "../../../../generated/prisma-client";
 
 export default {
   Mutation: {
-    requestSecret: async (_, args, { request }) => {
-      console.log(request);
+    requestSecret: async (_, args) => {
       const { email } = args;
       const loginSecret = generateSecret();
-      // console.log(loginSecret);
-
       try {
         await sendSecretMail(email, loginSecret);
         await prisma.updateUser({ data: { loginSecret }, where: { email } });
         return true;
-      } catch {
+      } catch (e) {
+        console.log(e);
         return false;
       }
     }
